c6d776da97f5039e0a07c22f64f621ec792acd9f
Pedram Veisi <pedramveisi@gmail.com>
2016-07-02 22:50:24 -0600

    Implement Product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,40 @@
 class Product
 
+    attr_reader :title, :price, :stock
     @@products = []
 
     def initialize(options={})
         @@products << self
+        
+        @title = options[:title] || ''
+        @price = options[:price] || 0
+        @stock = options[:stock] || 0
     end
 
     def self.all
         @@products
+    end
+
+    def self.find_by_title(title)
+        @@products.each do |product|
+            if product.title == title
+                return product
+            end
+        end
+
+        return nil
+    end
+
+    def in_stock?
+        if @stock != 0
+            return true
+        else
+            return false
+        end
+    end
+
+    def self.in_stock
+        @@products.select {|product| product.in_stock?}
     end
 
 end
